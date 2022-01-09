59e1056668635c632bd908fc8bde728652ec41e6
ChelseyG <86028043+ChelseyGravel@users.noreply.github.com>
2022-01-08 19:24:43 -0500

    Added another page

---
+++ js/js/index.js
@@ -0,0 +1,27 @@
+console.log("your indes.js file is loaded correctly!");
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+$(".modal-wide").on("show.bs.modal", function() {
+  var height = $(window).height() - 200;
+  $(this).find(".modal-body").css("max-height", height);
+});
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+  
